9d3b450f2b6c50ea3de360f1f6d04796268c6d6c
Jhonatan Guzman <jhonatan.guzman@endava.com>
2018-08-24 16:52:36 -0500

    Template

---
+++ bitbucket/src/main/java/com/endava/dashboard/bitbucket/BitbucketApplication.java
@@ -0,0 +1,12 @@
+package com.endava.dashboard.bitbucket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitbucketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitbucketApplication.class, args);
+	}
+}
